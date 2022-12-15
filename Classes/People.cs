44db073f74734648b53b671175af7e0c36c2d676
Denis Kozhukhar <mts701475@gmail.com>
2022-12-15 21:18:55 +0300

    Проект Classes

---
+++ Classes/People.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class People
+    {
+        public int age;
+        public string name;
+        public double weight;
+        public double longer;
+
+
+        public People() {
+            Console.WriteLine("Пустой экземпляр объекта успешно создан");
+        }
+
+        public People(int age, string name, double weight, double longer)
+        {
+            this.age = age;
+            this.name = name;
+            this.weight = weight;
+            this.longer = longer;
+
+            Console.WriteLine($"Hello, my name is {name}. My age is - {age}. Weight - {weight}кг!");
+        }
+
+        ~People() { }
+
+        public void Deconstruct(out int age, out string name) {
+            age = this.age;
+            name = this.name;
+        }
+
+            
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        // объкет -> класс(экземпляр объекта) -> переменная (экземпляр класса)
+
+        static void Main(string[] args)
+        {
+            People p = new People();
+
+            p.name = "Test";
+            p.age = 10;
+            p.weight = 10;
+            p.longer = 10;
+
+            People p2 = new People(27, "Igor", 50, 150);
+
+            //(int age, string name) = p2;  Синтаксический сахар
+
+            //if(i < n) z ++;
+            //  else z--;
+
+            int i = 3,  j = 4;
+            int z = 0;
+            z = (i < j) ? z++ : z--; // Синтаксический сахар условия if else (тернарная операция)
+
+            string name;
+            int age;
+
+            p2.Deconstruct(out age, out name);
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+
+            Console.ReadKey();
+        }
+
+    }
+
+}
